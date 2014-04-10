d93af7831ea25042d9a6467183feb361a359ce83
daidjassia <daidjassia@hotmail.com>
2014-04-10 21:14:08 +0200

    Première page, formulaires, et opérations

---
+++ tp/formulaire.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+</head>
+<body>
+
+<form action="formulaire.php" method="GET"/>
+nom:<input type="text" name="nom"/><br>
+prenom:<input type="text" name="prenom"/><br>
+numero_tel:<input type="text" name="numero de telephone"/><br>
+email:<input type="text" name="email"/><br>
+<input type="submit" name="valider"/>
+</form>
+</body>
+</html>

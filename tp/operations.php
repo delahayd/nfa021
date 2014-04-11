8206a9c75ef472504961eb7f11e1815f4b493996
maryse <maryse.chedanne@free.fr>
2014-04-11 14:02:24 +0200

    details

--- tp/operations.php
+++ tp/operations.php
@@ -2,8 +2,8 @@
 <head>
 </head>
 <body>
-<a href="formulaire.php"> <input type="button" name="ajouter" value="ajouter" /></a>
-<h1>rechercher</h1>
+<a href="formulaire.php"> <input type="button" name="ajouter" value="ajouter une personne" /></a>
+<h1>rechercher une personne</h1>
 <select>
 <option value="nom">nom</option>
 <option value="prnom">prenom</option>
